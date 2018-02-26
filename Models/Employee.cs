635c8e19a607643d4cffc67982c1df175401490a
Connor Greene <connor.greene2000@gmail.com>
2018-02-25 21:19:10 -0500

    Update SignalR client and minimum for Employee Name

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -6,7 +6,7 @@ namespace Vue2SpaSignalR.Models
     {
         public int ID { get; set; }
 
-        [StringLength(60, MinimumLength = 3)]
+        [StringLength(60, MinimumLength = 2)]
         public string Name { get; set; }
 
         [DataType(DataType.EmailAddress)]
